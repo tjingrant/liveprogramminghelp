62596cc63857a2bcb371a1412ed46715ad2805fb
tjin <tjingrant@gmail.com>
2015-02-21 01:48:29 -0500

    with payment gateway

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,13 @@
 class StaticController < ApplicationController
   def index
-  	
+
   end
 
   def about
   end
+
+  def payment
+	Rails.logger.info("PARAMS: #{params.inspect}")
+	render plain: "OK"
+  end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'static/index'
 
   get 'static/about'
-
+ 
+  get 'static/payment'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
