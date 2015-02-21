a06db86a7bd16e6b383a1db4f67f1df1c800e4df
tjin <tjingrant@gmail.com>
2015-02-21 01:49:49 -0500

    new notify url

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'static/about'
  
+  post 'static/payment'
   get 'static/payment'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
